2eeb9400fe6c1ab33cd811c14c2820a04dac3c7d
Joshua Donahue <jtdonahue360@gmail.com>
2015-10-15 11:58:04 -0400

    initial commit

---
+++ huffman.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "pq.h"
+
+using namespace std;
+
+int frequencies[256] = {0};
+
+int* buildFrequencyTable()
+
+void test() {
+	frequencies['a'] = 53;
+	frequencies['b'] = 22;
+
+	for(int i = 0; i<256; i++){
+		cout << frequencies[i] << " ";
+	}
+}
+
+
+int main(int argc, char* argv[]){
+	test();
+	return 0;
+}
