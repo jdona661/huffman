a545a575c486e0ab7a04a2540f408c7199b78707
Joshua Donahue <jtdonahue360@gmail.com>
2015-10-27 23:14:10 -0400

    add huffman.h

---
+++ huffman.h
@@ -0,0 +1,26 @@
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+#include <unordered_map>
+
+struct HuffmanNode;
+class Huffman {
+	HuffmanNode *root;
+	Huffman(const std::unordered_map<char, unsigned int> &charFreq);
+public:
+	Huffman() : root(nullptr) { ; }
+	~Huffman();
+	static Huffman buildTreeFromFile(const char *filename);
+	static Huffman readTreeFromFile(const char *filename);
+
+	// the following method throw Exception objects upon failure
+	void saveTreeToFile(const char *filename);
+	void compress(const char *source, const char *dest);
+	void decompress(const char *source, const char *dest);
+
+#ifdef TESTING
+	void print() const;
+	static void runUnitTests();
+#endif
+};
+
+#endif
